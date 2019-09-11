f6372180b06433d458e75680f9ff666d8950b122
Ahmed Abdelazim <toshiba20.ahmed@gmail.com>
2019-09-11 17:03:00 +0000

    added run.bat to run on windows

---
+++ run.bat
@@ -0,0 +1,13 @@
+ffmpeg -i intro.mp4 -r 30 -c:v libx265 -c:a libmp3lame -crf 22 -preset ultrafast -c:a libmp3lame intro-converted.mp4
+ffmpeg -i outro.mp4 -r 30 -c:v libx265 -c:a libmp3lame -crf 22 -preset ultrafast -c:a libmp3lame outro-converted.mp4
+cd input
+for %%f in (*.mp4) do (
+echo %%f
+ffmpeg -i %%f -i ..\overlay.png -filter_complex "[1:v]scale=520:-1,format=rgba,colorchannelmixer=aa=0.5[fg];[0][fg]overlay=1380:920:enable='between(t,0,20)'" -r 30 -pix_fmt yuv420p -c:v libx265 -preset ultrafast -crf 22  -c:a libmp3lame ..\temp-%%f
+echo file '..\intro-converted.mp4' > ..\list.txt
+echo file '..\temp-%%f' >> ..\list.txt
+echo file '..\outro-converted.mp4' >> ..\list.txt
+ffmpeg -f concat -safe 0 -i ..\list.txt -r 30 -pix_fmt yuv420p -c:v libx264 -preset ultrafast -crf 22  -c:a libmp3lame ..\output\%%f
+del ..\temp-%%f
+)
+cd ..
